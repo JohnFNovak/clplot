198262dd99d953c783d219a40779d1c436d30cdc
John Novak <john.franc.novak@gmail.com>
2013-09-11 14:44:55 -0400

    renamed plot.py main.py and moved functions into sub-modules

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ import string
 import subprocess
 import globe
 from structure import *
+from helpers import *
+from plot import *
 
 def main():
-    dic = globe.dic
+    global dic
 
     read_flags()
 
@@ -97,7 +99,7 @@ def main():
 
 def detect_blocks(dataarray):
     """This function runs over an array of data pulled from a file and detects the structure so that the proper plotting method can be deduced the structure is returned as a list. Each entry is one block of the data in the form of (width of block, height of block) This will detect contiguous rectangular blocks of data with the same formats of text vs numbers"""
-    dic = globe.dic
+    global dic
     
     width=[]
     height=[]
@@ -186,40 +188,6 @@ def detect_blocks(dataarray):
     return structure
 
 
-def is_it_ordered(vals):
-    """This function takes a list of numbers are returns whether or not they are in order"""
-
-    ordered = False
-
-    if vals == sorted(vals):
-        ordered = True
-    if vals == sorted(vals, reverse=True):
-        ordered = True
-
-    return ordered
-
-def remove_empties(struct,x):
-    """This function runs through the data and the structure array and removes entries that are either empty or are singular"""
-
-    linenum=len(x)-1
-    structBK=struct
-    count=0
-    blocks=len(structBK)
-
-    for i in range(blocks):
-        j=-i-1+count # do this backward
-        if structBK[j][0] > 1 and structBK[j][1] > 1:
-            # the entry is good: it's more than a single column or single line
-            linenum=linenum-struct[j][1]
-        else:
-            # the entry is worthless: it's a single line
-            del struct[len(structBK)+j]
-            del x[linenum]
-            linenum=linenum-1
-            count=count+1
-
-    return struct,x
-
 def readdat(struct,block,data):
     x=[]
     linenum=0
@@ -237,180 +205,6 @@ def readdat(struct,block,data):
     
     return x
 
-def plot(z,errs):
-    """This function takes a list z of lists and trys to plot them. the first list is always x, and the folowing are always y's"""
-
-    #print z
-    #print errs
-
-    dic = globe.dic
-    points=dic['colorstyle']
-
-    if dic['Ucolor']:
-        colors=dic['Ucolor']
-    else:
-        colors=['b','g','r','c','m','y','k']
-    if dic['Ustyle']:
-        style=dic['Ustyle']
-    else:
-        style=['o','v','^','<','>','1','2','3','4','-','--','-.',':','s','p','*','h','H','+','x','D','d','|','_','.',',']
-    for s in style:
-        for c in colors:
-            points.append(str(c+s))
-
-    size = [dic['default_marker_size']]*len(points)
-    for i in range(len(points)):
-        if len(points[i].split(';')) == 2:
-            points[i] = points[i].split(';')[0]
-            size[i] = float(points[i].split(';')[1])
-
-    if dic['MULTIT']:
-        dic['multicounttile'] = dic['multicounttile'] + 1
-        if not dic['columnsfirst']:
-            plt.subplot2grid((dic['layout'][0],dic['layout'][1]),(((dic['multicounttile']-1)-(dic['multicounttile']-1)%dic['layout'][1])/dic['layout'][1],((dic['multicounttile']-1)%dic['layout'][1])))
-        if dic['columnsfirst']:
-            plt.subplot2grid((dic['layout'][0],dic['layout'][1]),((dic['multicounttile']-1)%dic['layout'][1])),(((dic['multicounttile']-1)-(dic['multicounttile']-1)%dic['layout'][1])/dic['layout'][1])
-        #plt.title(str(dic['multicounttile']), fontsize = dic['fontsize'])
-        dic['LefttoPlot'] = True
-
-    plottingerrors = True
-    #for k in errs:
-    #    if k != 0:
-    #        plottingerrors = True
-
-    arg = []
-
-    if dic['x_range']:
-        plt.xlim(dic['x_range'])
-    if dic['y_range']:
-        plt.ylim(dic['y_range'])
-    if dic['x_label']:
-        plt.xlabel(dic['x_label'], fontsize = dic['fontsize'])
-    if dic['y_label']:
-        plt.ylabel(dic['y_label'], fontsize = dic['fontsize'])
-    if dic['x_log']:
-        plt.xscale('log', nonposx='clip')
-    if dic['y_log']:
-        plt.yscale('log', nonposy='clip')
-
-    plt.tick_params(axis='both', which='major', labelsize=dic['fontsize']*0.75)
-    plt.tick_params(axis='both', which='minor', labelsize=dic['fontsize']*0.75)
-
-    if dic['legend'] and len(dic['labels']) > 1:
-        parse_legend();
-
-    if dic['norm']:
-        for k in range(0,len(z),2):
-            X = np.array(z[k]).astype(float)
-            Y = np.array(z[k+1]).astype(float)
-            width = np.mean(X[1:] - X[:-1])
-            Y = Y / np.sum(Y * width)
-            z[k+1] = Y.tolist()
-
-    for k in range(0,len(z),2):
-        marker = points[((k+1)/2)%len(points)]
-        msize  = size[((k+1)/2)%len(points)]
-        ecolor = points[((k+1)/2)%len(points)][0]
-        fcolor = points[((k+1)/2)%len(points)][0]
-        if marker[-1] == '!':
-            fcolor = 'white'
-            marker = marker[:-1]
-        z[k]=map(lambda x: float(x) * dic['xscaled'],z[k])
-        z[k+1]=map(lambda x: float(x) * dic['yscaled'],z[k+1])
-        #z[k+1]=map(float,z[k+1])
-        if plottingerrors and not dic['errorbands']:
-            if errs[k][0] == 0:
-                errs[k][0]=[0]*len(z[k])
-            else:
-                errs[k][0]=map(lambda x: float(x) * dic['xscaled'],errs[k][0])
-            if errs[k+1][0] == 0:
-                plt.errorbar(z[k],z[k+1],xerr=errs[k][0],yerr=[0]*len(z[k+1]),fmt=marker,label=dic['labels'][(k+1)/2],mec=ecolor,mfc=fcolor,ms=msize)
-            if errs[k+1][0] != 0:
-                errs[k+1][0]=map(lambda x: float(x) * dic['yscaled'],errs[k+1][0])
-                plt.errorbar(z[k],z[k+1],xerr=errs[k][0],yerr=errs[k+1][0],fmt=marker,label=dic['labels'][(k+1)/2],mec=ecolor,mfc=fcolor,ms=msize)
-        if plottingerrors and dic['errorbands']:
-            if errs[k][0] == 0:
-                errs[k][0]=[0]*len(z[k])
-            else:
-                errs[k][0]=map(lambda x: float(x) * dic['xscaled'],errs[k][0])
-            if errs[k+1][0] == 0:
-                plt.errorbar(z[k],z[k+1],xerr=[0]*len(errs[k][0]),yerr=[0]*len(z[k+1]),fmt=marker,label=dic['labels'][(k+1)/2],mec=ecolor,mfc=fcolor,ms=msize)
-            if errs[k+1][0] != 0:
-                errs[k+1][0]=map(lambda x: float(x) * dic['yscaled'],errs[k+1][0])
-                plt.errorbar(z[k],z[k+1],xerr=[0]*len(errs[k][0]),yerr=[0]*len(errs[k+1][0]),fmt=marker,label=dic['labels'][(k+1)/2],mec=ecolor,mfc=fcolor,ms=msize)
-                plt.fill_between(np.array(z[k]),np.array(z[k+1])+np.array(errs[k+1][0]),np.array(z[k+1])-np.array(errs[k+1][0]),facecolor=ecolor,alpha=dic['alpha'],interpolate=True,linewidth=0)
-        if dic['plot_sys_err']:
-            plt.fill_between(np.array(z[k]),np.array(z[k+1])+np.array(errs[k+1][1]),np.array(z[k+1])-np.array(errs[k+1][1]),facecolor=ecolor,alpha=dic['alpha'],interpolate=True,linewidth=0)
-        if not plottingerrors:
-            arg.append(z[k]) # x vector
-            arg.append(z[k+1])
-            arg.append(points[(k/2)%len(points)])
-
-    if not plottingerrors:
-        plt.plot(*arg)
-
-    plt.grid(dic['grid'])
-
-    if dic['legend']:
-        plt.legend()
-        dic['labels']=[]
-
-    if dic['currentoutput']:
-        outputname = dic['currentoutput']
-    else:
-        outputname = string.split(dic['currentfile'],".")[0]
-
-    if dic['numbered'] != 0:
-        outputname = outputname+"_"+str(dic['numbered'])
-    if dic['MULTIT']:
-        outputname = outputname+"_tiled"
-    if dic['multicountpile'] != 0:
-        outputname = outputname+"_"+str(dic['multicountpile'])
-    if dic['MULTIP']:
-        outputname = outputname+"_multip"
-    if dic['TYPE'][0]==".":
-        outputname=outputname+dic['TYPE']
-    else:
-        outputname=outputname+"."+dic['TYPE']
-
-    if not dic['MULTIT'] or (dic['MULTIT'] and dic['multicounttile'] == int(dic['MULTIT'])):
-        plt.tight_layout() # Experimental, and may cause problems
-        plt.savefig(outputname)
-        print"printed to",outputname
-        f = open(outputname,'a')
-        f.write("Creation time: "+time.ctime()+'\n')
-        f.write("Current directory: "+os.path.abspath('.')+'\n')
-        f.write("Creation command: "+' '.join(sys.argv)+'\n')
-        f.write("Plotted values:"+'\n')
-        for k in range(0,len(z),2):
-            f.write('x '+' '.join(map(str,z[k]))+'\n')
-            f.write('x err '+' '.join(map(str,errs[k]))+'\n')
-            f.write('y '+' '.join(map(str,z[k+1]))+'\n')
-            f.write('y err '+' '.join(map(str,errs[k+1]))+'\n')
-        f.close()
-        #check = subprocess.call(['open',outputname])
-        plt.clf()
-        dic['LefttoPlot'] = False
-
-    if dic['MULTIT'] and dic['multicounttile'] == int(dic['MULTIT']):
-            dic['multicounttile'] = 0
-
-
-def check_type(x):
-    """This function returns a string. It returns "str" if x is a string, and "num" if x is a number"""
-
-    try:
-        float(x)
-    except ValueError:
-        verdict="str"
-    else:
-        verdict="num"
-
-    return verdict
-
-def skip(iterator, n):
-    '''Advance the iterator n-steps ahead. If n is none, consume entirely.'''
-    collections.deque(itertools.islice(iterator, n), maxlen=0)
 
 def read_data(filename):
     data=[]
@@ -439,24 +233,9 @@ def read_data(filename):
     data = remove_formating(data)
     return data
 
-def remove_formating(data):
-    """This function removes thigns that will cause problems like endlines"""
-    cleaned=[]
-    for i in data:
-        temp=[]
-        for j in i:
-            temp2=[]
-            for k in j:
-                if (k != '\n') and (k != '') and (k!='\r'):
-                    temp2.append(k)
-            if(len(temp2)>0):
-                temp.append(string.join(temp2,''))
-        cleaned.append(temp)
-
-    return cleaned
 
 def read_flags():
-    dic = globe.dic
+    global dic
     case = 0 # 0 is reading files, 1 is outputs, 2 is formats, etc
 
     if len(sys.argv)==1:
@@ -583,7 +362,7 @@ def read_flags():
                 print "ignoring",flag
 
 def parse_legend():
-    dic = globe.dic
+    global dic
     #delimiters = ['/','-','.','/','-','.']
     delimiters = ['/','-']
 
@@ -677,4 +456,7 @@ def givehelp(a):
 
 
 if __name__ == '__main__':
-    print "This code is part of CLP"
+    """A Python program that takes a file or list of filesand creates plots of the data."""
+    global dic # All global values are being dumped in here
+    dic = globe.dic
+    main()
