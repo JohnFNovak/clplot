5747a05280729c69e2aa228bdf9e102c551edaa7
John Novak <john.franc.novak@gmail.com>
2013-09-11 14:32:30 -0400

    moved global dictionary to separate sub-module

---
+++ globe.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Part of CLP
+# A universal command line plotting script
+#
+# John Novak
+
+# This sub-module does some of the automatic structure determination
+
+# written for Python 2.6. Requires Scipy, Numpy, and Matplotlib
+
+import sys
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+import os
+import time
+import string
+import subprocess
+
+
+dic = { 'formats':[],'outputs':[],'TYPE':'pdf','MULTIT':None,'MULTIP':None,'layout':None,'columnsfirst':False,'Ucolor':[],'Ustyle':[],'Messy':False,'remnants':[],'remnanterrors':[],'LefttoPlot':False,'x_range':None,'y_range':None,'x_label':None,'y_label':None,'x_log':False,'y_log':False,'currentfile':None,'numbered':None,'Numbering':None,'multicounttile':0,'multicountpile':0,'currentoutput':None,'files':[],'legend':False,'labels':[],'columnlabel':[],'currentstruct':0,'colorstyle':[],'errorbands':False, 'fontsize':20, 'grid':False,'sys_err_default':0,'default_marker_size':5,'sys_err':0,'plot_sys_err':False, 'yscaled':1, 'xscaled':1, 'alpha':0.25, 'norm':False}
+
+
+if __name__ == '__main__':
+    print "This code is part of CLP"
--- plot.py
+++ plot.py
@@ -19,6 +19,7 @@ import os
 import time
 import string
 import subprocess
+import globe
 
 def main():
     global dic
@@ -1023,5 +1024,5 @@ def givehelp(a):
 if __name__ == '__main__':
     """A Python program that takes a file or list of filesand creates plots of the data."""
     global dic # All global values are being dumped in here
-    dic = { 'formats':[],'outputs':[],'TYPE':'pdf','MULTIT':None,'MULTIP':None,'layout':None,'columnsfirst':False,'Ucolor':[],'Ustyle':[],'Messy':False,'remnants':[],'remnanterrors':[],'LefttoPlot':False,'x_range':None,'y_range':None,'x_label':None,'y_label':None,'x_log':False,'y_log':False,'currentfile':None,'numbered':None,'Numbering':None,'multicounttile':0,'multicountpile':0,'currentoutput':None,'files':[],'legend':False,'labels':[],'columnlabel':[],'currentstruct':0,'colorstyle':[],'errorbands':False, 'fontsize':20, 'grid':False,'sys_err_default':0,'default_marker_size':5,'sys_err':0,'plot_sys_err':False, 'yscaled':1, 'xscaled':1, 'alpha':0.25, 'norm':False}
+    dic = globe.dic
     main()
