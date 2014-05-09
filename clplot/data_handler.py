fae48975e3aee79015600113dd27366a90913754
JohnFNovak <john.franc.novak@gmail.com>
2014-05-09 14:30:26 -0400

    more stupid pep8 comliance with strings that I don't like

--- clplot/data_handler.py
+++ clplot/data_handler.py
@@ -15,11 +15,7 @@ from helpers import *
 
 
 def detect_blocks(dataarray):
-    """This function runs over an array of data pulled from a file and \
-detects the structure so that the proper plotting method can be deduced the \
-structure is returned as a list. Each entry is one block of the data in the \
-form of (width of block,  height of block) This will detect contiguous \
-rectangular blocks of data with the same formats of text vs numbers"""
+    """This function runs over an array of data pulled from a file and detects the structure so that the proper plotting method can be deduced the structure is returned as a list. Each entry is one block of the data in the form of (width of block,  height of block) This will detect contiguous rectangular blocks of data with the same formats of text vs numbers"""
     dic = globe.dic
 
     width = []
