07ab3e1c331435ae0f74cc70398c35f1880533f4
JohnFNovak <john.franc.novak@gmail.com>
2014-05-27 15:15:59 -0400

    added barebones class for lines and line plots

---
+++ clplot/class_LinePlot.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+# Part of CLP
+# A universal command line plotting script
+#
+# John Novak
+
+# this sub-file holds the line plots class
+
+
+class LinePlot():
+    """This class represents 2d point and line plots"""
+    def __init__(self):
+        pass
+
+
+class Line():
+    """This class represents a single line in a LinePlot"""
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    print "This code is part of CLP"
